e9fbb653077f036716a82677e79cdf6e38c2c34e
Jose Bayona <bayonaj368@gmail.com>
2023-01-12 12:23:15 -0600

    folder structure and java matrix challenge completed

---
+++ java/ThreadRunnableInterface/src/TestThread.java
@@ -0,0 +1,138 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chrio
+ */
+class RunnableDemo implements Runnable {
+
+    private Thread t;
+    private final String threadName;
+     private final char [][] matrix ={
+            { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z' },
+            { 'p', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'q' },
+            { 'h', 'h', 'e', 'l', 'l', 'o', 'u', 'i', 'o', 'p' },
+            { 'e', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'o', 'z' },
+            { 'l', 'e', 'r', 'h', 'e', 'l', 'l', 'o', 'l', 'q' },
+            { 'l', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z' },
+            { 'o', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'e', 'q' },
+            { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'h', 'z' },
+            { 'w', 'e', 'o', 'l', 'l', 'e', 'h', 'o', 'p', 'q' },
+            { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z' }
+    };
+
+    RunnableDemo(String name) {
+        threadName = name;
+        System.out.println("Creating " + threadName);
+    }
+
+    public void run() {
+
+        System.out.println("Running " + threadName);
+
+        switch (threadName) {
+            case "left to right" -> {
+
+                for (char[] chars : matrix) {
+
+                    String joinedArray = new String(chars);
+
+                    if (joinedArray.contains("hello")) {
+
+                        System.out.println("Found hello in " + threadName + " *******");
+
+                    }
+
+                }
+            }
+            case "right to left" -> {
+
+                for (char[] chars : matrix) {
+
+                    String joinedArray = new String(chars);
+
+                    if (joinedArray.contains("olleh")) {
+
+                        System.out.println("Found hello in " + threadName + " *******");
+
+                    }
+
+                }
+            }
+            case "top to bottom" -> {
+
+                for (int i = 0; i < matrix[0].length; i++) {
+
+                    StringBuilder txt = new StringBuilder();
+
+                    for (char[] chars : matrix) {
+
+                        txt.append(chars[i]);
+
+                    }
+
+                    if (txt.toString().contains("hello")) {
+
+                        System.out.println("Found hello in " + threadName + " *******");
+
+                    }
+
+                }
+            }
+            case "bottom to top" -> {
+
+                for (int i = 0; i < matrix[0].length; i++) {
+
+                    StringBuilder txt = new StringBuilder();
+
+                    for (char[] chars : matrix) {
+
+                        txt.append(chars[i]);
+
+                    }
+
+                    if (txt.toString().contains("olleh")) {
+
+                        System.out.println("Found hello in " + threadName + " *******");
+
+                    }
+
+                }
+            }
+        }
+
+        System.out.println( "Thread " + threadName + " exiting." );
+
+    }
+
+    public void start() {
+        System.out.println("Starting " + threadName);
+
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
+
+public class TestThread {
+
+    public static void main(String[] args) {
+
+
+        RunnableDemo leftToRightThread = new RunnableDemo( "left to right" );
+        RunnableDemo rightToLeftThread = new RunnableDemo( "right to left" );
+        RunnableDemo topToBottom       = new RunnableDemo( "top to bottom" );
+        RunnableDemo bottomToTop       = new RunnableDemo( "bottom to top" );
+
+        leftToRightThread.start();
+        rightToLeftThread.start();
+        topToBottom.start();
+        bottomToTop.start();
+
+    }
+    
+}
---
+++ java/ThreadThreadClass/src/TestThread.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chrio
+ */
+class ThreadDemo extends Thread {
+
+    private Thread t;
+    private String threadName;
+
+    ThreadDemo(String name) {
+        threadName = name;
+        System.out.println("Creating " + threadName);
+    }
+
+    public void run() {
+        /*
+        This method provides an entry point for the thread and you will put your 
+        complete business logic inside this method.
+         */
+        System.out.println("Running " + threadName);
+
+        try {
+
+            for (int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+
+                // Let the thread sleep for a while.
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + threadName + " interrupted.");
+        }
+        System.out.println("Thread " + threadName + " exiting.");
+    }
+
+    public void start() {
+        System.out.println("Starting " + threadName);
+
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
+
+public class TestThread {
+
+    public static void main(String args[]) {
+        ThreadDemo T1 = new ThreadDemo("Thread-1");
+        T1.start();
+
+        ThreadDemo T2 = new ThreadDemo("Thread-2");
+        T2.start();
+    }
+}
