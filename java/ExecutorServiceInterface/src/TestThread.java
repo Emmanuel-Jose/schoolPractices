b93150b8c7e312ea71f1031eace3c1fadbc7fcdc
Jose Bayona <bayonaj368@gmail.com>
2023-02-22 08:38:54 -0600

    check db connectivity with executors

--- java/ExecutorServiceInterface/src/TestThread.java
+++ java/ExecutorServiceInterface/src/TestThread.java
@@ -7,20 +7,22 @@
  *
  * @author chrio
  */
+import java.sql.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ScheduledExecutorService;
+
 
 public class TestThread {
 
     public static void main(final String[] arguments) throws InterruptedException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
+//        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
         try {
             executor.submit(new Task());
-            System.out.println("Shutdown executor");
-            executor.shutdown();
-            executor.awaitTermination(5, TimeUnit.SECONDS);
+            executor.awaitTermination(60, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             System.err.println("tasks interrupted");
         } finally {
@@ -37,13 +39,33 @@ public class TestThread {
 
         @Override
         public void run() {
-
+            // check if the database exists
             try {
-                Long duration = (long) (Math.random() * 20);
-                System.out.println("Running Task!");
-                TimeUnit.SECONDS.sleep(duration);
+                while(true){
+                    Connection conn = null;
+                    Statement st = null;
+                    Class.forName("com.mysql.jdbc.Driver");
+                    String pass = "kiop4.5F#";
+                    String user = "root";
+                    String db = "test";
+                    String port = "3306";
+                    conn = DriverManager.getConnection("jdbc:mysql://localhost:" + port + "/" + db, user, pass);
+                    st = conn.createStatement();
+                    String sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '" + db + "'";
+                    ResultSet rs = st.executeQuery(sql);
+
+                    if (rs.next()) {
+                        System.out.println("Database exists");
+                    }
+                    Thread.sleep(5000);
+                }
+
+            } catch (ClassNotFoundException ignored) {
+
+            } catch (SQLException ex) {
+                System.out.println("Database does not exist");
             } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
             }
         }
     }
