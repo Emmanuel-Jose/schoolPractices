235b0fb943d0d51d51e24ea34394d0109d97b707
Jose Bayona <bayonaj368@gmail.com>
2023-03-08 09:05:11 -0600

    ADD: automata project

--- react/automata-practice/src/components/InputAutomata.tsx
+++ react/automata-practice/src/components/InputAutomata.tsx
@@ -1,13 +1,12 @@
 import {FormEvent, useEffect, useState} from "react";
 import {InputAutomataProps} from "../types/types";
 import {validateDFA} from "../helpers/validateDFA";
-import { DFA3, DFA5 } from "../helpers/DFAData";
+import { DFA1, DFA2, DFA3, DFA4, DFA5, DFA6, DFA7 } from "../helpers/DFAData";
 
 
 export const InputAutomata = ({ isAutomataValid, automataType, onChangeMessageDisplayed }: InputAutomataProps ): JSX.Element => {
 
     const [ automataInput, setAutomataInput ] = useState<string>('');
-    // const [ inputMessage, setInputMessage ] = useState<string>('');
 
     useEffect(()=> {
         setAutomataInput('');
@@ -16,7 +15,6 @@ export const InputAutomata = ({ isAutomataValid, automataType, onChangeMessageDi
 
     useEffect(()=> {
         if ( automataInput === '' ) {
-            // if there is no input, don't show the message
             onChangeMessageDisplayed(false);
             return;
         }
@@ -33,35 +31,38 @@ export const InputAutomata = ({ isAutomataValid, automataType, onChangeMessageDi
         if ( automataInput === '' ) return;
         validateAutomata();
         if ( automataInput !== '' ) onChangeMessageDisplayed(true);
-        // if ( !hasOnlyLetters(automataInput) ) onChangeMessageDisplayed(false);
     }
 
     const validateAutomata = (): void => {
         switch ( automataType ) {
-            // case 1:
-            //     const isValidDFA1 = validateDFA( DFA1, automataInput );
-            //     isAutomataValid(isValidDFA1);
-            //     break;
-            // case 2:
-            //     const isValidDFA2 = validateDFA( DFA2, automataInput );
-            //     isAutomataValid(isValidDFA2);
-            //     break;
+            case 1:
+                const isValidDFA1 = validateDFA( DFA1, automataInput );
+                isAutomataValid(isValidDFA1);
+                break;
+            case 2:
+                const isValidDFA2 = validateDFA( DFA2, automataInput );
+                isAutomataValid(isValidDFA2);
+                break;
             case 3:
                 const isValidDFA3 = validateDFA( DFA3, automataInput );
                 isAutomataValid(isValidDFA3);
                 break;
-            // case 4:
-            //     const isValidDFA4 = validateDFA( DFA4, automataInput );
-            //     isAutomataValid(isValidDFA4);
-            //     break;
+            case 4:
+                const isValidDFA4 = validateDFA( DFA4, automataInput );
+                isAutomataValid(isValidDFA4);
+                break;
             case 5:
                 const isValidDFA5 = validateDFA( DFA5, automataInput );
                 isAutomataValid(isValidDFA5);
                 break;
-            // case 6:
-            //     const isValidDFA6 = validateDFA( DFA6, automataInput );
-            //     isAutomataValid(isValidDFA6);
-            //     break;
+            case 6:
+                const isValidDFA6 = validateDFA( DFA6, automataInput );
+                isAutomataValid(isValidDFA6);
+                break;
+            case 7:
+                const isValidDFA7 = validateDFA( DFA7, automataInput );
+                isAutomataValid(isValidDFA7);
+                break;
         }
     }
 
@@ -80,10 +81,6 @@ export const InputAutomata = ({ isAutomataValid, automataType, onChangeMessageDi
                         className='w-5/6 sm:w-3/4 md:w-1/2 h-10 sm:h-12 bg-neutral-800 text-neutral-100 rounded-md focus:outline-none px-2.5'
                         placeholder='Enter your string here...'
                     />
-                    {/* <p
-                        className={`${ ( inputMessage === 'valid string' ) ? 'text-green-500' : 'text-red-500' } text-xs -mt-5`}
-                    >{ ( automataInput === '' ) ? '' : inputMessage }</p> */}
-
 
                 <button
                     type='submit'
--- react/automata-practice/src/helpers/DFAData.ts
+++ react/automata-practice/src/helpers/DFAData.ts
@@ -1,83 +1,91 @@
 import {DFA} from '../types/types';
 
-// export const DFA1: DFA = {
-//     startState: 'q0',
-//     finalStates: [ 'q3' ],
-//     transitions: {
-//         q0: {
-//             a: 'q1',
-//             b: 'q2',
-//             c: 'q3'
-//         },
-//         q1: {
-//             a: 'q2',
-//             b: 'q3',
-//             c: 'q1'
-//         },
-//         q2: {
-//             a: 'q3',
-//             b: 'q1',
-//             c: 'q3'
-//         }
-//     }
-// }
 
-// export const DFA2: DFA = {
-//     startState: 'q0',
-//     finalStates: [ 'q3', 'q4', 'q6', 'q8' ],
-//     transitions: {
-//         q0: {
-//             a: 'q4',
-//             c: 'q1'
-//         },
-//         q1: {
-//             c: 'q3'
-//         },
-//         q4: {
-//             a: 'q4',
-//             b: 'q8',
-//             c: 'q1'
-//         },
-//         q6: {
-//             b: 'q8',
-//         },
-//         q8: {
-//             b: 'q8',
-//         }
-//     }
-// }
+export const DFA1: DFA = {
+    startState: 'q1',
+    finalStates: [ 'q6' ],
+    transitions: {
+        q1: {
+            0: 'q2',
+        },
+        q2: {
+            0: 'q2',
+            1: 'q3',
+        },
+        q3: {
+            0: 'q3',
+            1: 'q4'
+        },
+        q4: {
+            0: 'q5'
+        },
+        q5: {
+            0: 'q5',
+            1: 'q6'
+        }
+    }
+}
 
-// export const DFA3: DFA = {
-//     startState: 'q0',
-//     finalStates: [ 'q7', 'q8' ],
-//     transitions: {
-//         q0: {
-//             a: 'q7',
-//         },
-//         q7: {
-//             a: 'q7',
-//             b: 'q8',
-//         },
-//         q8: {
-//             b: 'q8',
-//             c: 'q4'
-//         },
-//         q4: {
-//             b: 'q8',
-//             c: 'q4'
-//         }
-//     }
-// }
 
+export const DFA2: DFA = {
+    startState: 'q1',
+    finalStates: [ 'q4' ],
+    transitions: {
+        q1: {
+            0: 'q1',
+            1: 'q2'
+        },
+        q2: {
+            0: 'q2',
+            1: 'q3',
+            ' ': 'q1'
+        },
+        q3: {
+            0: 'q4',
+            1: 'q3'
+        }
+    }
+}
 
 
 export const DFA3: DFA = {
     startState: 'q1',
-    finalStates: [ 'q1' ],
+    finalStates: [ 'q3' ],
     transitions: {
         q1: {
-            a: 'q1',
-            b: 'q1',
+            a: 'q2',
+        },
+        q2: {
+            a: 'q2',
+            b: 'q3',
+        },
+        q3: {
+            ' ': 'q1'
+        }
+    }
+}
+
+
+export const DFA4: DFA = {
+    startState: 'q1',
+    finalStates: [ 'q6' ],
+    transitions: {
+        q1: {
+            a: 'q2',
+        },
+        q2: {
+            a: 'q2',
+            b: 'q3',
+        },
+        q3: {
+            a: 'q4',
+            ' ': 'q1'
+        },
+        q4: {
+            b: 'q5',
+        },
+        q5: {
+            b: 'q6',
         }
     }
 }
@@ -97,29 +105,76 @@ export const DFA5: DFA = {
 }
 
 
-// export const DFA6: DFA = {
-//     startState: 'q1',
-//     finalStates: [ 'q7' ],
-//     transitions: {
-//         q1: {
-//             a: 'q2',
-//         },
-//         q2: {
-//             a: 'q3',
-//         },
-//         q3: {
-//             b: 'q4',
-//             "": 'q1'
-//         },
-//         q4: {
-//             b: 'q5',
-//         },
-//         q5: {
-//             b: 'q6',
-//         },
-//         q6: {
-//             a: 'q7',
-//             "": 'q5'
-//         }
-//     }
-// }
+export const DFA6: DFA = {
+    startState: 'q1',
+    finalStates: [ 'q7' ],
+    transitions: {
+        q1: {
+            a: 'q2',
+        },
+        q2: {
+            a: 'q3',
+        },
+        q3: {
+            ' ': 'q1',
+            b: 'q4',
+        },
+        q4: {
+            b: 'q5',
+        },
+        q5: {
+            b: 'q6',
+            ' ': 'q3'
+        },
+        q6: {
+            b: 'q6',
+            a: 'q7'
+        }
+    }
+}
+
+
+export const DFA7: DFA = {
+    startState: 'q1',
+    finalStates: [ 'q11' ],
+    transitions: {
+        q1: {
+            a: 'q2',
+        },
+        q2: {
+            a: 'q3',
+        },
+        q3: {
+            a: 'q4',
+            ' ': 'q2'
+        },
+        q4: {
+            b: 'q5',    
+        },
+        q5: {
+            b: 'q6',
+            ' ': 'q4'
+        },
+        q6: {
+            a: 'q7',
+        },
+        q7: {
+            a: 'q7',
+            b: 'q8',
+        },
+        q8: {
+            b: 'q9',
+        },
+        q9: {
+            b: 'q10',
+            ' ': 'q1'
+        },
+        q10: {
+            b: 'q11',
+        },
+        q11: {
+            a: 'q11',
+            b: 'q11'
+        }
+    }
+}
